bb984126231ebdafd5da211765dd18b5e55d6a65
NEANG SAM OUN <NEANG SAM OUN@DESKTOP-DFTBN9T>
2025-06-22 20:51:22 +0700

    config host

--- Program.cs
+++ Program.cs
@@ -13,6 +13,7 @@ namespace WebApplication2
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.WebHost.UseUrls("http://0.0.0.0:80");
 
             var app = builder.Build();
 
